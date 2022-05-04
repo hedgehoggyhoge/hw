fdcef2902441b5ef3fd81b286af3942c6b085169
Anastasia Shirobokova <anastasiia.shirobokova@gmail.com>
2022-05-04 17:28:21 +0300

    HW1 is completed!!!

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"golang.org/x/example/stringutil"
+	"io"
+	"os"
+)
+
+func Reverse(s string) string {
+	return stringutil.Reverse(s)
+}
+
 func main() {
-	// Place your code here.
+	_, err := io.WriteString(os.Stdout, Reverse("Hello, OTUS!"))
+	if err != nil {
+		panic(err)
+	}
 }
